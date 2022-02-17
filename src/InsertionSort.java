e9e1022614c2d42c545bc6a69ecb8f13ac220222
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-02-17 17:37:41 +0100

    Added delay to better see the algorithm in action

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,3 +1,4 @@
+import javax.swing.SwingWorker;
 
 public class InsertionSort extends SortingAlgorithm {		//O(n^2), best case O(n)
 	
@@ -8,29 +9,40 @@ public class InsertionSort extends SortingAlgorithm {		//O(n^2), best case O(n)
 	}
 
 	@Override
-	void sort(SortingOrder so) {
-		for(int i = 1; i < ar.length; i++) {
-			int temp = Main.bars[i].value;
-			int j = i - 1;
-			
-			if(so == SortingOrder.LtoH) {
-				while(j >= 0 && ar[j].value > temp) {
-					Main.bars[j+1].value = Main.bars[j].value;
+	void sort(SortingOrder so, int delay) {
+		SwingWorker<Void, String> Worker = new SwingWorker<Void, String>(){
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				for(int i = 1; i < ar.length; i++) {
+					int temp = Main.bars[i].value;
+					int j = i - 1;
+					
+					if(so == SortingOrder.LtoH) {
+						while(j >= 0 && ar[j].value > temp) {
+							Main.bars[j+1].value = Main.bars[j].value;
+							Main.bars[j+1].panel.setBounds(0,Settings.allBarsPanelHeigth-Main.bars[j+1].value,1000,Main.bars[j+1].value);
+							Thread.sleep(delay);
+							j--;
+						}		
+					} else if(so == SortingOrder.HtoL) {
+						while(j >= 0 && ar[j].value < temp) {
+							Main.bars[j+1].value = Main.bars[j].value;
+							Main.bars[j+1].panel.setBounds(0,Settings.allBarsPanelHeigth-Main.bars[j+1].value,1000,Main.bars[j+1].value);
+							Thread.sleep(delay);
+							j--;
+						}	
+					}
+					
+					ar[j+1].value = temp;
+					Main.bars[j+1].value = temp;
 					Main.bars[j+1].panel.setBounds(0,Settings.allBarsPanelHeigth-Main.bars[j+1].value,1000,Main.bars[j+1].value);
-					j--;
-				}		
-			} else if(so == SortingOrder.HtoL) {
-				while(j >= 0 && ar[j].value < temp) {
-					Main.bars[j+1].value = Main.bars[j].value;
-					Main.bars[j+1].panel.setBounds(0,Settings.allBarsPanelHeigth-Main.bars[j+1].value,1000,Main.bars[j+1].value);
-					j--;
-				}	
+					Thread.sleep(delay);
+				}
+				return null;
 			}
-			
-			ar[j+1].value = temp;
-			Main.bars[j+1].value = temp;
-			Main.bars[j+1].panel.setBounds(0,Settings.allBarsPanelHeigth-Main.bars[j+1].value,1000,Main.bars[j+1].value);
-		}
+      };
+      Worker.execute();	
 		
 	}
 
