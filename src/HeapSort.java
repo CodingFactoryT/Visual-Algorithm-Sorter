bedfb522f4ac4620e41367f2d9cf429fab3fc037
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-02-26 22:56:56 +0100

    changed delay so it can be updated at runtime

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -9,33 +9,33 @@ public class HeapSort extends SortingAlgorithm{
 	}
 	
 	@Override
-	void sort(SortingOrder so, int delay) {
+	void sort(SortingOrder so) {
 		SwingWorker<Void, String> Worker = new SwingWorker<Void, String>(){
 
 			@Override
 			protected Void doInBackground() throws Exception {
-				heapSort(so, ar, delay);
+				heapSort(so, ar);
 				return null;
 			}
       };
       Worker.execute();
 	}
 	
-	void heapSort(SortingOrder so, Bar[] array, int delay) throws Exception {
+	void heapSort(SortingOrder so, Bar[] array) throws Exception {
 		int size = array.length;
 		
 		for(int i = size/2-1; i>=0; i--) {
-			heapify(so, array, size, i, delay);
+			heapify(so, array, size, i);
 		}
 		
 		for(int i=size-1; i>=0; i--) {
 			swapBars(array[0], array[i]);
-			Thread.sleep(delay);
-			heapify(so, array, i, 0, delay);
+			Thread.sleep(Settings.sortingDelay);
+			heapify(so, array, i, 0);
 		}		
 	}
 	
-	void heapify(SortingOrder so, Bar array[], int size, int i, int delay) throws Exception{
+	void heapify(SortingOrder so, Bar array[], int size, int i) throws Exception{
 		int largest = i;
 		int l = 2*i+1;
 		int r = 2*i+2;
@@ -59,8 +59,8 @@ public class HeapSort extends SortingAlgorithm{
 		
 		if(largest != i) {
 			swapBars(array[i], array[largest]);
-			Thread.sleep(delay);
-			heapify(so, array, size, largest, delay);
+			Thread.sleep(Settings.sortingDelay);
+			heapify(so, array, size, largest);
 		}
 		
 	}
